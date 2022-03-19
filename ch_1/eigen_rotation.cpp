74a864c9bd1c7dc1b4b1eab7613f5f85d679f482
Bozhang Ma <bozhang.ma@tum.de>
2022-03-19 22:26:36 +0100

    finish ch1

--- ch_1/eigen_rotation.cpp
+++ ch_1/eigen_rotation.cpp
@@ -1,44 +1,99 @@
 #include <iostream>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
-#include <sophus/so3.cpp>
+#include <sophus/se3.hpp>
+#include <sophus/so3.hpp>
 
-/**
- * @brief Eigen::UnitRandom(), ref: http://planning.cs.uiuc.edu/node198.html
- * @return
- */
-Eigen::Quaterniond unit_random() {
-    double u1 = rand() / double(RAND_MAX); // [0, 1]
-    double u2 = rand() / double(RAND_MAX) * M_2_PI;
-    double u3 = rand() / double(RAND_MAX) * M_2_PI;
-    double a = std::sqrt(1 - u1);
-    double b = std::sqrt(u1);
-    return Eigen::Quaterniond(a*sin(u2), a*cos(u2), b*sin(u3), b*cos(u3)).normalized();
+typedef Sophus::SE3d SE3;
+typedef Sophus::SO3d SO3;
+using namespace std;
+
+
+// 旋转矩阵更新方法一：通过旋转向量计算exp(w^)
+Eigen::Matrix3d computeMatrixByAngleAxis(Eigen::Vector3d w) 
+{
+    // 得到角度，即旋转向量的模长
+    double angle = sqrt(w(0) * w(0) + w(1) * w(1) + w(2) * w(2));
+    // 得到轴，即将旋转向量归一化
+    Eigen::Vector3d axis = w / angle;
+    // 定义旋转向量
+    Eigen::AngleAxisd r_angleaxis = Eigen::AngleAxisd(angle, axis);
+    // 通过旋转向量计算exp(w^)
+    Eigen::Matrix3d expw = r_angleaxis.toRotationMatrix();
+    return expw;
 }
 
+// 旋转矩阵更新方法二：通过Sophus提供的函数计算exp(w^)
+Eigen::Matrix3d computeMatrixBySophus(Eigen::Vector3d w) 
+{
+    // 通过Sophus提供的函数直接计算exp(w^)
+    Sophus::SO3d expw_sophus = Sophus::SO3d::exp(w);
+    Eigen::Matrix3d expw = expw_sophus.matrix();
+    return expw;
+}
+
+// 旋转矩阵更新方法三：通过罗德里格斯公式计算exp(w^)
+Eigen::Matrix3d computeMatrixByRodrigues(Eigen::Vector3d w) 
+{
+    // 单位矩阵
+    Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
+    // 得到角度，即旋转向量的模长
+    double alpha = sqrt(w(0) * w(0) + w(1) * w(1) + w(2) * w(2));
+    // 得到轴，即将旋转向量归一化
+    Eigen::Vector3d n = w / alpha;
+    // 矩阵n^
+    Eigen::Matrix3d skew_n;
+    skew_n << 0, -n(2), n(1),
+              n(2), 0, -n(0),
+              -n(1), n(0), 0;
+    // 使用罗德里格斯公式计算对应的旋转矩阵exp(w^)
+    // R = cos(alpha)I + (1-cos(alpha))nn^T + sin(alpha)n^
+    Eigen::Matrix3d expw;
+    expw = cos(alpha) * I + (1 - cos(alpha)) * n * n.transpose() + sin(alpha) * skew_n;
+    return expw;
+}
+
+// 主函数
 int main()
 {
-    // Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI_2, Eigen::Vector3d(0,0,1)).toRotationMatrix();
-    Eigen::Matrix3d R(unit_random());
-    std::cout << "the random initial rotation matrix R:\n" << R << std::endl << std::endl;
+    // 定义一个旋转向量
+    Eigen::AngleAxisd r_angleaxis = Eigen::AngleAxisd(M_PI/4, Eigen::Vector3d(0, 0, 1));
+    // 从旋转向量生成旋转矩阵R
+    Eigen::Matrix3d R = r_angleaxis.toRotationMatrix();
+    // 从旋转向量生成四元数q
+    Eigen::Quaterniond q = Eigen::Quaterniond(r_angleaxis);
 
-    Sophus::SO3 R_SO3(R);
-    Eigen::Vector3d omega(0.01, 0.02, 0.03);
-    Sophus::SO3 R_SO3_updated = R_SO3 * Sophus::SO3::exp(omega);
-    Eigen::Matrix3d R1 = R_SO3_updated.matrix();
-    std::cout << "R1:\n" << R1 << std::endl << std::endl;
+    // 定义一个旋转的更新量w
+    Eigen::Vector3d w(0.01, 0.02, 0.03);
 
-    Eigen::Quaterniond R_q(R);
-    Eigen::Quaterniond q_update(1, omega[0]*0.5, omega[1]*0.5, omega[2]*0.5);
-    Eigen::Quaterniond R_q_updated = (R_q*q_update).normalized();
-    Eigen::Matrix3d R2 = R_q_updated.toRotationMatrix();
-    std::cout << "R2:\n" << R2 << std::endl << std::endl;
+    // 计算旋转的更新量对应的旋转矩阵，即exp(w^)
+    Eigen::Matrix3d expw1 = computeMatrixByAngleAxis(w);
+    Eigen::Matrix3d expw2 = computeMatrixBySophus(w);
+    Eigen::Matrix3d expw3 = computeMatrixByRodrigues(w);
+    // 更新旋转矩阵: R <- R * exp(w^)
+    Eigen::Matrix3d R_new1 = R * expw1;
+    Eigen::Matrix3d R_new2 = R * expw2;
+    Eigen::Matrix3d R_new3 = R * expw3;
 
-    Eigen::Matrix3d R_error = R1 - R2;
-    std::cout << "error matrix (R1-R2):\n" << R_error << std::endl << std::endl;
+    // 计算旋转的更新量对应的四元数，即[1, 1/2w]^T
+    Eigen::Quaterniond q_update(1, 0.5*w(0), 0.5*w(1), 0.5*w(2));
+    // 更新四元数：q <- q * [1, 1/2w]^T
+    Eigen::Quaterniond q_new = q * q_update;
+    Eigen::Matrix3d R_new_q = q_new.toRotationMatrix();
 
-    double error_f_norm = R_error.norm();
-    std::cout << "the Frobenius Norm of the error matrix: " << error_f_norm << std::endl;
+    // 显示结果的对比
+    cout << "Update using rotation matrix:" << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "use AngleAxis: " << endl << R_new1 << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "use Sophus: " << endl << R_new2 << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "use Rodrigues: " << endl << R_new3 << endl;
+    cout << "-------------------------------------" << endl;
+    cout << endl;
+    cout << "Update using quaternion:" << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "use quaternion: " << endl << R_new_q << endl;
 
     return 0;
 }
